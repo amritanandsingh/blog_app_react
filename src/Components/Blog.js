ad545a816540ce0b59294a7dff75eb77fd22ed0c
Amrit Anand Singh <amritanandsingh@Amrits-MacBook-Air.local>
2023-07-21 01:20:50 +0530

    first

---
+++ src/Components/Blog.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+//Blogging App using Hooks
+export default function Blog(){
+    // const [title, setTitle] = useState("");
+    // const [content, setContent] = useState("");
+
+    const [formData , setFormData ] = useState({title:"",content:""});
+
+    const [Blog, setBlog] = useState([]);
+    
+    //Passing the synthetic event as argument to stop refreshing the page on submit
+    function handleSubmit(e) {
+        e.preventDefault();
+        //const newBlog = { title, content };
+        // setTitle('');
+        // setContent('');
+        setBlog([ formData,...Blog]); // Use spread operator to append the new blog to the existing array
+        setFormData.title="";
+        setFormData.content="";
+    }
+
+    return(
+        <>
+        {/* Heading of the page */}
+        <h1>Write a Blog!</h1>
+
+        {/* Division created to provide styling of section to the form */}
+        <div className="section">
+
+        {/* Form for to write the blog */}
+            <form onSubmit={handleSubmit}>
+
+                {/* Row component to create a row for first input field */}
+                <Row label="Title">
+                        <input className="input"
+                                placeholder="Enter the Title of the Blog here.."
+                                value={formData.title}
+                                onChange={(e)=>setFormData({title:e.target.value})}
+                                />
+                </Row >
+
+                {/* Row component to create a row for Text area field */}
+                <Row label="Content">
+                        <textarea className="input content"
+                                placeholder="Content of the Blog goes here.."
+                                value={formData.content}
+                                onChange={(e)=>setFormData({content: e.target.value})}
+                                />
+                </Row >
+
+                {/* Button to submit the blog */}            
+                <button className = "btn">ADD</button>
+            </form>
+                     
+        </div>
+
+        <hr/>
+
+        {/* Section where submitted blogs will be displayed */}
+        <h2> Blogs </h2>
+            {
+            Blog.map((blog, i) => (
+                <div className="blog" key={i}>
+                <h3>{blog.title}</h3>
+                <p>{blog.content}</p>
+                </div>
+            ))
+            }
+        </>
+        )
+}
+
+//Row component to introduce a new row section in the form
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <label>{label}<br/></label>
+        {props.children}
+        <hr />
+        </>
+    )
+}
